80a9f5453c8449ba5e000f9c49f0d822f33e1e2b
Faruk Yucel <farukyucel5@gmail.com>
2022-12-12 23:03:40 +0300

    polimorphism

---
+++ Overriding/Overriding/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //method overriding = provides a new version of a method inherited from a parent class
+            //                    inherited method must be: abstract, virtual, or already overriden
+            //                    Used with ToString(), polymorphism 
+
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            Cat lion = new Lion();
+
+            dog.Speak();
+            cat.Speak();
+            lion.Speak();
+
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal goes *brrr*");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog goes *woof*");
+        }
+    }
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The cat goes meow");
+        }
+
+    }
+
+    class Lion : Cat
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The lion roars");
+        }
+    }
+}
