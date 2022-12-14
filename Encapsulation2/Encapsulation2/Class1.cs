d920d61adb69d85636b2109b23d89c5aeac55d4a
Faruk Yucel <farukyucel5@gmail.com>
2022-12-14 22:52:05 +0300

    updating

---
+++ Encapsulation2/Encapsulation2/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation2
+{
+   public class Class1
+    {
+
+        private int price=20000;
+
+        private int production_year=2022;
+
+        
+
+
+      public int Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 20000)
+                {
+                    price = 20000;
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public int Production_year
+        {
+            get { return production_year; }
+        }
+  }
+}
---
+++ Encapsulation2/Encapsulation2/Encaps.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Encapsulation2
+{
+     class Encaps
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Class1 object1 = new Class1();
+
+            Console.WriteLine(object1.Production_year);
+            Console.WriteLine(object1.Price);
+
+            object1.Price = 13000;
+
+            Console.WriteLine(object1.Price);
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
